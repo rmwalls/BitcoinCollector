4a0c41b9d22d871ec93a84fd45e50a2db2b4dca7
ZelleM <groovyrlm@gmail.com>
2019-05-09 17:39:17 -0400

    Moved js into separate file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,41 @@
-  function startMe() {
-      
-    // Creates an array that lists out all of the computer's options, aka, the alphabet
-    var computerChoices = ("abcdefghijklmnopqrstuvwxyz").split("");
-
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log("Computer chose: " + computerGuess);
-
-    // Creating  and setting variables to hold the number of wins, losses. They start at 0. # of guesses at 9.
-    // array to hold keys guessed
-    var wins = 0;
-    var losses = 0;
-    var guess = 9;
-    var keysGuessed = [];
-
-    // Create variables that hold references to the places in the HTML where we want to display game stats.
-    var directionsText = document.getElementById("directions-text");
-    var userChoiceText = document.getElementById("userchoice-text");
-    var computerChoiceText = document.getElementById("computerchoice-text");
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
-    var guessText = document.getElementById("guessLeft-text");
-    var userGuessText = document.getElementById("userGuess-text");
-
-    document.onkeyup = function (event) {
-
-      var userGuess = event.key; // Determines which key was pressed.
-      if(keysGuessed.includes(userGuess)) {
-      return;
-      }
-      keysGuessed.push(userGuess); // add the user's guess to the array, unless already there
-
-      // Determine outcome
-      if (userGuess === computerGuess) { // user wins, do they want to play again?
-          console.log("you won");
-          wins++;
-          var playAgain = prompt("You're PSYCHIC! Play again?");
-          if (playAgain != null) { // subif
-            startMe();
-          } else {
-            alert("OK, y'all come back now, ya hear?");
-            keysGuessed = [];
-            guess = "";
-          } // end subif
-
-      } else if ((userGuess !== computerGuess) && (guess > 0)) { // user guess is no match, but lives left
-        guess--;
-        console.log("elseif " + guess);
-      } else { // no match, out of lives
-        losses++;
-        guess = 9;
-        console.log("else " + guess);
-        var newGame = prompt("Sorry! Out of guesses. Play again?");
-          if (newGame != null) {
-          console.log("startover " + newGame);
-          startMe(); // starts game at the top
-        } else {
-          alert("Bye, bye! Thanks for playing!"); // clear out stats
-          wins = 0;
-          losses = 0;
-          guess = 0;
-          keysGuessed = [];
-        } 
-      } // end of outcome
-
-      // Hide the directions
-      directionsText.textContent = "";
-
-      // Display the user and computer guesses, and wins/losses/remaining guesses.
-      userChoiceText.textContent = "You chose: " + userGuess;
-      winsText.textContent = "Wins: " + wins;
-      lossesText.textContent = "Losses: " + losses;
-      guessText.textContent = "Guesses left: " + guess; //reset when user guesses right or is wrong 9 times
-      userGuessText.textContent = "Your guesses so far: " + keysGuessed // document.write(keysGuessed);
-    } //end onkeyup 
-  } // end function startMe
-
+      function startGame() {
+        var wins=0;
+        var losses=0;
+        var yourScore=0;
+        var winsText = document.getElementById("wins-text");
+        winsText.textContent = "Wins: " + wins;
+        var lossesText = document.getElementById("losses-text"); 
+        lossesText.textContent = "Losses: " + losses;
   
+	var numToMatch = Math.floor(Math.random() * (120 - 19) + 19); 
+            console.log(numToMatch + " is the match number");
+	    $("#random-number").text(numToMatch);
+
+        //Generate random number for each bitcoin
+        var bit1= Math.floor(Math.random()*12+1);
+        console.log("#1 is " + bit1);
+        var bit2= Math.floor(Math.random()*12+1);
+        console.log("#2 is " + bit2);
+        var bit3= Math.floor(Math.random()*12+1);
+        console.log("#3 is " + bit3);
+        var bit4= Math.floor(Math.random()*12+1);
+        console.log("#4 is " + bit4); 
+	
+	playGame();     
+
+       function playGame() {
+        $("#button-one").on("click", function(){
+        yourScore = yourScore + bit1;
+        console.log(yourScore);
+   })
+        if (yourScore === numToMatch) {
+          alert("you win");
+          wins++
+          resetGame();
+         } else if (yourScore > numToMatch) {
+            alert("loser");
+            losses++
+            resetGame();
+          } else {
+            alert("update score");
+	}}}
