c0171f07c87759f1b6f5cecf47f6dd5982127c9b
ZelleM <groovyrlm@gmail.com>
2019-05-10 14:42:04 -0400

    Only 2 more fixes left!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,95 @@
-      function startGame() {
-        var wins=0;
-        var losses=0;
-        var yourScore=0;
-        var winsText = document.getElementById("wins-text");
-        winsText.textContent = "Wins: " + wins;
-        var lossesText = document.getElementById("losses-text"); 
-        lossesText.textContent = "Losses: " + losses;
+function startGame() {
+var wins=0;
+  var losses=0;
+  var yourScore=0;
+  var outcome="are Playing!";
+  var winsText = document.getElementById("wins-text");
+  winsText.textContent = "Wins: " + wins;
+
+  var lossesText = document.getElementById("losses-text"); 
+  lossesText.textContent = "Losses: " + losses;
+
+  var outcomeText = document.getElementById("outcome-text");
+  outcomeText.textContent = "You " + outcome;
+  
   
 	var numToMatch = Math.floor(Math.random() * (120 - 19) + 19); 
-            console.log(numToMatch + " is the match number");
-	    $("#random-number").text(numToMatch);
+    console.log(numToMatch + " is the match number");
+  $("#random-number").text(numToMatch);
 
-        //Generate random number for each bitcoin
-        var bit1= Math.floor(Math.random()*12+1);
-        console.log("#1 is " + bit1);
-        var bit2= Math.floor(Math.random()*12+1);
-        console.log("#2 is " + bit2);
-        var bit3= Math.floor(Math.random()*12+1);
-        console.log("#3 is " + bit3);
-        var bit4= Math.floor(Math.random()*12+1);
-        console.log("#4 is " + bit4); 
+  //Generate random number for each bitcoin
+  var bit1= Math.floor(Math.random()*12+1);
+  console.log("#1 is " + bit1);
+  var bit2= Math.floor(Math.random()*12+1);
+  console.log("#2 is " + bit2);
+  var bit3= Math.floor(Math.random()*12+1);
+  console.log("#3 is " + bit3);
+  var bit4= Math.floor(Math.random()*12+1);
+  console.log("#4 is " + bit4); 
 	
-	playGame();     
+	 
+  $("#button-one").on("click", function(){
+  yourScore = yourScore + bit1;
+  $("#calculated-score").text(yourScore);
+  console.log(yourScore);
+  didIWin();
+  })
 
-       function playGame() {
-        $("#button-one").on("click", function(){
-        yourScore = yourScore + bit1;
-        console.log(yourScore);
-   })
-        if (yourScore === numToMatch) {
-          alert("you win");
-          wins++
-          resetGame();
-         } else if (yourScore > numToMatch) {
-            alert("loser");
-            losses++
-            resetGame();
-          } else {
-            alert("update score");
-	}}}
+  $("#button-two").on("click", function(){
+  yourScore = yourScore + bit2;
+  $("#calculated-score").text(yourScore);
+  console.log(yourScore);
+  didIWin();
+  })
+
+  $("#button-three").on("click", function(){
+  yourScore = yourScore + bit3;
+  $("#calculated-score").text(yourScore);
+  console.log(yourScore);
+  didIWin();
+  })
+
+  $("#button-four").on("click", function(){
+  yourScore = yourScore + bit4;
+  $("#calculated-score").text(yourScore);
+  console.log(yourScore);
+  didIWin();
+  })
+
+  function resetGame(){
+    numToMatch = Math.floor(Math.random() * (120 - 19) + 19); 
+    console.log(numToMatch + " is the new match number");
+    $("#random-number").text(numToMatch);
+     //Regenerate random number for each bitcoin
+      bit1= Math.floor(Math.random()*12+1);
+      console.log("#1 is " + bit1);
+      bit2= Math.floor(Math.random()*12+1);
+      console.log("#2 is " + bit2);
+      bit3= Math.floor(Math.random()*12+1);
+      console.log("#3 is " + bit3);
+      bit4= Math.floor(Math.random()*12+1);
+      console.log("#4 is " + bit4); 
+    yourScore = 0;
+    outcome = "are playing!";
+    outcomeText.textContent = "You " + outcome;
+  }
+
+  function didIWin(){
+    if (yourScore === numToMatch) {
+    outcome = "won!"
+    outcomeText.textContent = "You " + outcome; 
+    prompt("You won. Play again!");
+    wins++;
+    winsText.textContent = "Wins: " + wins;
+    resetGame();
+    } else if (yourScore > numToMatch) {
+    outcome = "lost";
+    outcomeText.textContent = "You " + outcome;
+    alert("Sorry! You lost. Try again")
+    losses++
+    lossesText.textContent = "Losses: " + losses;
+    resetGame();
+    } else {
+    console.log("keep going");
+    
+          }}}
